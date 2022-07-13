e803581bbd504c839083c3c041c82064fdc558ce
fanghs <fanghs@citycloud.com.cn>
2022-07-13 15:40:18 +0800

    实现computed

---
+++ src/reactivity/computed.ts
@@ -0,0 +1,28 @@
+import { ReactiveEffect } from "./effect"
+
+class ComputedRefImpl {
+  private _getter: any
+  private _dirty: boolean = true
+  private _value: any
+  private _effect: any
+  constructor(getter) {
+    this._getter = getter
+    //  当依赖的响应式对象的值发生改变的时候，使用调度器scheduler把dirty锁打开，并切不会二次触发effect
+    this._effect = new ReactiveEffect(getter, () => {
+      if (!this._dirty) {
+        this._dirty = true
+      }
+    })
+  }
+  get value() {
+    if (this._dirty) {
+      this._dirty = false
+      this._value = this._effect.run()
+    }
+    return this._value
+  }
+}
+
+export function computed(getter) {
+  return new ComputedRefImpl(getter)
+}
