a21ff540d60037ea73ef88ed0fbee3d263bf0f7c
fanghs <fanghs@citycloud.com.cn>
2022-07-20 16:11:09 +0800

    实现element主流程

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -1,14 +1,28 @@
 import { h } from "../../lib/guide-mini-vue.esm.js"
 
-export const App ={
-  render(){
-    return h("div","hi, " + this.msg)
+export const App = {
+  render() {
+    return h(
+      "div",
+      {
+        id:'root',
+        class:["red","hard"]
+      },
+      // "hi, " + this.msg
+      // string
+      // "hi,mini-vue"
+      // array
+      [
+        h('div',{class:['red']},"hi"),
+        h("div",{class:['blue']},"mini-vue")
+      ]
+    )
   },
 
-  setup(){
+  setup() {
     // composition api
     return {
-      msg:'mini-vue'
+      msg: 'mini-vue'
     }
   }
 }
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,36 +1,67 @@
+import { isObject } from "../shared/index"
 import { createComponentInstance, setUpComponent } from "./component"
 
-export function render(vnode,container){
+export function render(vnode, container) {
   // patch 
   // 
-  patch(vnode,container)
+  patch(vnode, container)
 }
 
-function patch(vnode,container){
+function patch(vnode, container) {
   // 处理element
-  // processElement()
-
-  // 去处理组件
-  processComponent(vnode , container)
+  if (typeof vnode.type === 'string') {
+    processElement(vnode,container)
+  } else if (isObject(vnode.type)) {
+    // 去处理组件
+    processComponent(vnode, container)
+  }
 }
 
-function processComponent(vnode:any,container:any){
-  mountComponent(vnode,container)
+function processElement(vnode:any,container:any){
+  mountElement(vnode,container)
 }
 
-function mountComponent(vndoe,container){
-  const instance =  createComponentInstance(vndoe)
+function mountElement(vnode:any,container:any){
+  const {type,props, children} = vnode
+  const el = document.createElement(type)
+  if(typeof children === 'string'){
+    el.textContent = children
+  }else if(Array.isArray(children)){
+    // children.forEach(child=>{
+    //   patch(child,el)
+    // })
+    mountChildren(vnode,el)
+  }
+  for(const key in props){
+    const val = props[key]
+    el.setAttribute(key,val)
+  }
+  container.append(el)
+}
+
+function mountChildren(vnode,container){
+  vnode.children.forEach(child=>{
+    patch(child,container)
+  })
+}
+
+function processComponent(vnode: any, container: any) {
+  mountComponent(vnode, container)
+}
+
+function mountComponent(vndoe, container) {
+  const instance = createComponentInstance(vndoe)
 
   setUpComponent(instance)
 
-  setupRenderEffect(instance,container)
+  setupRenderEffect(instance, container)
 }
 
-function setupRenderEffect(instance,container){
+function setupRenderEffect(instance, container) {
   // subTree 虚拟节点树
   const subTree = instance.render()
 
   // vnode -> patch
   // vnode -> element -> mountElement
-  patch(subTree,container)
+  patch(subTree, container)
 }
